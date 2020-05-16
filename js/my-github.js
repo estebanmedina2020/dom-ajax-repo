5a2d3f8dd58476db832dad31a5d6f3d9c81a0b2f
Esteban <emedina@creatonico.com>
2020-05-16 19:41:58 +0200

    finish exercise Dom-Ajax repo part 1

--- js/my-github.js
+++ js/my-github.js
@@ -1,1 +1,23 @@
 // Write code here to communicate with Github
+fetch("https://api.github.com/users/estebanmedina2020/repos", {
+  method: "get"
+})
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    var reposCountNode = document.querySelector("#repos-count");
+    reposCountNode.innerText = data.length;
+    var reposListNode = document.querySelector("#repos-list");
+    data.forEach(element => {
+      const reposListElement = document.createElement("li");
+      const anchorElement = document.createElement("a");
+      anchorElement.setAttribute("href", element.url);
+      anchorElement.innerHTML = element.name;
+      reposListElement.appendChild(anchorElement);
+      reposListNode.appendChild(reposListElement);
+    });
+  })
+  .catch(function(err) {
+    // Error :(
+  });
